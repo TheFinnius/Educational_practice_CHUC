d95512764686dd07b2fe8b1ff3ebec1a256fe413
Сергей <92118466+TheFinnius@users.noreply.github.com>
2021-12-04 05:44:31 +0500

    Задание 4
    Выполнено

--- Educational_practice_CHUC/Educational_practice_CHUC/Zadaniye4.Designer.cs
+++ Educational_practice_CHUC/Educational_practice_CHUC/Zadaniye4.Designer.cs
@@ -28,19 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(398, 426);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseClick);
             // 
             // Zadaniye4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(422, 450);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Zadaniye4";
             this.Text = "Zadaniye4";
+            this.Load += new System.EventHandler(this.Zadaniye4_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- Educational_practice_CHUC/Educational_practice_CHUC/Zadaniye4.cs
+++ Educational_practice_CHUC/Educational_practice_CHUC/Zadaniye4.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Educational_practice_CHUC
 {
@@ -15,6 +16,40 @@ namespace Educational_practice_CHUC
         public Zadaniye4()
         {
             InitializeComponent();
+        }
+
+        private void Zadaniye4_Load(object sender, EventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection(ConnectBDPRO.ConnectBD());
+            string sql = $"SELECT idStud, fioStud, drStud FROM t_datetime";
+            try
+            {
+                conn.Open();
+                MySqlDataAdapter IDataAdapter = new MySqlDataAdapter(sql, conn);
+                DataSet dataset = new DataSet();
+                IDataAdapter.Fill(dataset);
+                dataGridView1.DataSource = dataset.Tables[0];
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        string id = "0";
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (!e.RowIndex.Equals(-1) && !e.ColumnIndex.Equals(-1) && e.Button.Equals(MouseButtons.Left))
+            {
+                dataGridView1.CurrentCell = dataGridView1[e.ColumnIndex, e.RowIndex];
+                dataGridView1.CurrentRow.Selected = true;
+                string index_rows5;
+                index_rows5 = dataGridView1.SelectedCells[0].RowIndex.ToString();
+                id = dataGridView1.Rows[Convert.ToInt32(index_rows5)].Cells[2].Value.ToString();
+                DateTime x = DateTime.Today;
+                DateTime y = Convert.ToDateTime(dataGridView1.Rows[Convert.ToInt32(index_rows5)].Cells[2].Value.ToString());
+                string resultDays = (x - y).ToString();
+                MessageBox.Show("Прошло " + resultDays.Substring(0, resultDays.Length - 9) + " дней(я) со дня рождения!");
+            }
         }
     }
 }
